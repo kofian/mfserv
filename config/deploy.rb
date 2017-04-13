a61124d2957c7a2523d4fb85f0e7b0b5b2c6f76c
kofian <deployer@server.traupdate.com>
2017-04-13 16:07:33 +0400

    tirdE

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,36 +18,15 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
-Rake::Task["deploy:assets:precompile"].clear_actions
-Rake::Task["deploy:assets:backup_manifest"].clear_actions
+#Rake::Task["deploy:assets:precompile"].clear_actions
+#Rake::Task["deploy:assets:backup_manifest"].clear_actions
 
-set :pty,             true
+set :passenger_restart_with_touch, true
+#set :pty,             true
 #set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 
-#set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, false 
-
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
@@ -61,25 +40,6 @@ namespace :deploy do
     end
   end
 
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
 end
 
 
@@ -90,7 +50,7 @@ end
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
 #set :deploy_to, "/var/www/transa-transact.com/public_html/money"
-set :deploy_to, "/var/www/money/public_html"
+set :deploy_to, "/var/www/transa-transact.com/public_html"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
