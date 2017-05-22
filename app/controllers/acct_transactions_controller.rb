a7da9c9c14e5d3be085ac60481cf78a63e435966
kofian <deployer@server.traupdate.com>
2017-05-22 17:56:22 +0400

    cleaning

--- app/controllers/acct_transactions_controller.rb
+++ app/controllers/acct_transactions_controller.rb
@@ -22,11 +22,11 @@ class AcctTransactionsController < ApplicationController
   def new
       @acct_transaction = AcctTransaction.new
       #@acct_transaction.id = SecureRandom.random_number(99999999999999)
-      @acct_transaction.account_id = params[:account_id]
-      @acct_transaction.date = Time.now
-      @acct_transaction.transaction_type_id = params[:transaction_type_id]
-      @acct_transaction.amount = params[:amount]
-      @wire_transfer = @acct_transaction.wire_transfers.new
+     # @acct_transaction.account_id = params[:account_id]
+      #@acct_transaction.date = Time.now
+      #@acct_transaction.transaction_type_id = params[:transaction_type_id]
+      #@acct_transaction.amount = params[:amount]
+      #@wire_transfer = @acct_transaction.wire_transfers.new
     # for the view - show 'name' of transaction types
     @trans_type = TransactionType.find(params[:transaction_type_id]).name
   end
@@ -38,9 +38,9 @@ class AcctTransactionsController < ApplicationController
   # POST /acct_transactions
   # POST /acct_transactions.json
   def create
-    @acct_transaction = AcctTransaction.new(acct_transaction_params)
+    #@acct_transaction = AcctTransaction.new(acct_transaction_params)
     #@acct_transaction.id = SecureRandom.random_number(99999999999999)
-    @acct_transaction.date = Time.now
+    #@acct_transaction.date = Time.now
     #@wire_transfer = @acct_transaction.wire_transfers.build
     #@acct_transaction.wire_transfers.routing = params[:routing]
     if @acct_transaction.valid?
