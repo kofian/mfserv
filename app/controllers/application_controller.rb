5a7790135c35b7afc3a4f52b791deba604169d11
kofian <al.abdimi@gmail.com>
2017-10-23 16:15:47 +0000

    application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
 
   	if current_user.role == 'admin'
-  		adminview_administrator_path(current_user, format: :html)
+  		adminview_admin_administrator_path(current_user, format: :html)
   	else
   		accounts_path(current_user, format: :html)
   	end
