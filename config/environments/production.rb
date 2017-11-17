835060de9a69628e9c8c17725e79208944ecb315
kofian <al.abdimi@gmail.com>
2017-11-17 07:52:06 +0000

    sass up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,10 +57,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "money_#{Rails.env}"
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'prodimain.ml'}
+  config.action_mailer.default_url_options = { host: 'meijerfinance.com'}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@prodimain.ml'}
+  config.action_mailer.default_options = {from: 'no-reply@meijerfinance.com'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
